74acc976e08d815cf37a1aaa27c17893c5e71a4c
James Drake <info@jamesmdrakemedia.com>
2025-06-11 16:26:18 -0400

    feat: scroll behavior and nav links

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-window.addEventListener("scroll", function (event) {
-  const topDistance = this.window.pageYOffset;
-  const layers = this.document.querySelectorAll("[data-type='parallax']");
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function (event) {
+    const topDistance = this.window.pageYOffset;
+    const layers = this.document.querySelectorAll("[data-type='parallax']");
 
-  for (let layer of layers) {
-    const depth = layer.getAttribute("data-depth");
-    const movement = -(topDistance * depth);
-    const translate3d = "translate3d(0, " + movement + "px, 0)";
-    layer.style["-webkit-transform"] = translate3d;
-    layer.style["-moz-transform"] = translate3d;
-    layer.style["-ms-transform"] = translate3d;
-    layer.style["-o-transform"] = translate3d;
-    layer.style.transform = translate3d;
-  }
+    for (let layer of layers) {
+      const depth = layer.getAttribute("data-depth");
+      const movement = -(topDistance * depth);
+      const translate3d = "translate3d(0, " + movement + "px, 0)";
+      layer.style["-webkit-transform"] = translate3d;
+      layer.style["-moz-transform"] = translate3d;
+      layer.style["-ms-transform"] = translate3d;
+      layer.style["-o-transform"] = translate3d;
+      layer.style.transform = translate3d;
+    }
+
+    const hero = document.getElementById("hero");
+    const heroHeight = hero ? hero.offsetHeight : 0;
+    const header = document.querySelector("header");
+    const headerHeight = header ? header.offsetHeight : 0;
+
+    if (header) {
+      console.log(topDistance, heroHeight - headerHeight);
+      if (topDistance > heroHeight - headerHeight) {
+        header.classList.add("header-bg");
+      } else {
+        header.classList.remove("header-bg");
+      }
+    }
+  });
 });
